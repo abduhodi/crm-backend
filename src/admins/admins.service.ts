153f9ac9d66c7219b6dd3b255735e24514599ed4
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2023-10-12 11:47:45 +0500

    return type changed

--- src/admins/admins.service.ts
+++ src/admins/admins.service.ts
@@ -50,17 +50,17 @@ export class AdminService {
 
   async findAllStudents() {
     const users = await this.userModel.find({ role: 'student' });
-    if (!users.length) {
-      throw new NotFoundException('Students are not found');
-    }
+    // if (!users.length) {
+    //   throw new NotFoundException('Students are not found');
+    // }
     return { students: users };
   }
 
   async findAllTeachers() {
     const users = await this.userModel.find({ role: 'teacher' });
-    if (!users.length) {
-      throw new NotFoundException('Teachers are not found');
-    }
+    // if (!users.length) {
+    //   throw new NotFoundException('Teachers are not found');
+    // }
     return { students: users };
   }
 
@@ -82,9 +82,9 @@ export class AdminService {
       throw new BadRequestException('Invalid id');
     }
     const user = await this.userModel.findOne({ id, role: 'student' });
-    if (!user) {
-      throw new NotFoundException('User is not found');
-    }
+    // if (!user) {
+    //   throw new NotFoundException('User is not found');
+    // }
     return { student: user };
   }
 
@@ -94,9 +94,9 @@ export class AdminService {
       throw new BadRequestException('Invalid id');
     }
     const user = await this.userModel.findOne({ id, role: 'teacher' });
-    if (!user) {
-      throw new NotFoundException('Teacher is not found');
-    }
+    // if (!user) {
+    //   throw new NotFoundException('Teacher is not found');
+    // }
     return { teacher: user };
   }
 
