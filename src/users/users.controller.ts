dde4294daa04c0c36e5e91ff64916b78bdb4e488
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2023-10-19 10:34:35 +0500

    change password handler, update profile handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import {
   UploadedFile,
   ParseFilePipe,
   Req,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -30,6 +31,8 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { ValidFileValidator } from '../validators/file.validator';
 import { Request } from 'express';
 import { FileUploadDto } from './dto/file-upload.dto';
+import { UpdateProfileDto } from './dto/profile-update.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @ApiTags('Uploads')
 @UseGuards(AuthGuard)
@@ -91,5 +94,50 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   getProfileInfo(@Req() req: Request) {
     return this.usersService.getProfileInfo(req);
+  }
+
+  //-------------- UPDATE PROFILE --------------------//
+
+  @ApiOperation({ summary: 'Update Profile' })
+  @ApiResponse({
+    status: HttpStatus.ACCEPTED,
+    description: 'succesfully updated',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'User is not found or invalid id',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Token is not found',
+  })
+  @Put('update')
+  @HttpCode(HttpStatus.ACCEPTED)
+  updateStudent(@Req() req: any, @Body() updateProfileDto: UpdateProfileDto) {
+    return this.usersService.updateProfile(req, updateProfileDto);
+  }
+
+  //-------------- CHANGE PASSWORD --------------------//
+
+  @ApiOperation({ summary: 'Change password' })
+  @ApiResponse({
+    status: HttpStatus.ACCEPTED,
+    description: 'succesfully updated',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'User is not found or invalid id',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Token is not found',
+  })
+  @Post('change-password')
+  @HttpCode(HttpStatus.ACCEPTED)
+  changePassword(
+    @Req() req: any,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    return this.usersService.changePassword(req, changePasswordDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,10 @@ import { uploadFile } from '../utils/file-upload';
 import { Model, isValidObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateProfileDto } from './dto/profile-update.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -36,6 +40,8 @@ export class UsersService {
     return user;
   }
 
+  //----------------------- GET PROFILE INFO -----------------------------//
+
   async getProfileInfo(req: any) {
     const id = req?.user?.id;
     const valid = isValidObjectId(id);
@@ -47,5 +53,56 @@ export class UsersService {
       throw new BadRequestException('User is not found');
     }
     return user;
+  }
+
+  //----------------------- CHANGE PASSWORD -----------------------------//
+
+  async changePassword(req: any, changePasswordDto: ChangePasswordDto) {
+    const { old_password, new_password, confirm_password } = changePasswordDto;
+    if (new_password !== confirm_password) {
+      throw new BadRequestException("Confiem password didn't match");
+    }
+    const id = req?.user?.id;
+    const valid = isValidObjectId(id);
+    if (!valid) {
+      throw new BadRequestException('Invalid id');
+    }
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new BadRequestException('User is not found');
+    }
+    const isMatch = bcrypt.compareSync(old_password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException("Password didn't match");
+    }
+    const hashedPassword = bcrypt.hashSync(new_password, 7);
+    const update = await this.userModel.updateOne(
+      { _id: id },
+      { password: hashedPassword },
+    );
+    return { update: update.modifiedCount ? 'success' : 'failed' };
+  }
+
+  //----------------------- UPDATE USER -----------------------------//
+
+  async updateProfile(req: any, updateUserDto: UpdateProfileDto) {
+    const id = req?.user?.id;
+    const valid = isValidObjectId(id);
+    if (!valid) {
+      throw new BadRequestException('Invalid id');
+    }
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new BadRequestException('User is not found');
+    }
+    const exist = await this.userModel.findOne({ phone: updateUserDto.phone });
+    if (exist && user?.id !== exist?.id) {
+      throw new BadRequestException('Phone number is already registered');
+    }
+    await user.updateOne(updateUserDto);
+    const updatedUser = await this.userModel
+      .findById(id)
+      .select('-password -token');
+    return { updated: updatedUser };
   }
 }
