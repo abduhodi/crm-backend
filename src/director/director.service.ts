e81292bad283fdd5f05b214d7153268bd3ce7dc6
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2023-10-20 10:59:26 +0500

    error resolved

--- src/director/director.service.ts
+++ src/director/director.service.ts
@@ -15,33 +15,9 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 export class DirectorService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async createTeacher(createUserDto: CreateUserDto) {
-    const user = await this.userModel.findOne({ phone: createUserDto.phone });
-    if (user)
-      throw new BadRequestException('Phone number is already registered');
-    return this.userModel.create({
-      ...createUserDto,
-      role: 'teacher',
-      password: bcrypt.hashSync(createUserDto.phone, 7),
-    });
-  }
-
-  async createAdmin(createUserDto: CreateUserDto) {
-    const user = await this.userModel.findOne({ phone: createUserDto.phone });
-    if (user)
-      throw new BadRequestException('Phone number is already registered');
-
-    return this.userModel.create({
-      ...createUserDto,
-      role: 'admin',
-      image: '',
-      password: bcrypt.hashSync(createUserDto.phone, 7),
-    });
-  }
-
   async activateAdmin(id: string) {
     const user = await this.userModel.findById(id).select('-password -token');
-    if (!user) throw new BadRequestException('Admin is not found');
+    if (!user) throw new BadRequestException('Staff is not found');
 
     user.status = !user.status;
     await user.save();
@@ -62,52 +38,5 @@ export class DirectorService {
       .limit(limit1);
 
     return { staffs };
-  }
-  async findAllStudents() {
-    const users = await this.userModel.find({ role: 'student' });
-
-    return { students: users };
-  }
-
-  async findAllAdmins() {
-    const users = await this.userModel.find({ role: 'admin' });
-
-    return { students: users };
-  }
-
-  async findAllTeachers() {
-    const users = await this.userModel.find({ role: 'teacher' });
-
-    return { students: users };
-  }
-
-  async findOneAdmin(id: string) {
-    const valid = isValidObjectId(id);
-    if (!valid) {
-      throw new BadRequestException('Invalid id');
-    }
-    const user = await this.userModel.findOne({ id, role: 'admin' });
-
-    return { admin: user };
-  }
-
-  async findOneStudent(id: string) {
-    const valid = isValidObjectId(id);
-    if (!valid) {
-      throw new BadRequestException('Invalid id');
-    }
-    const user = await this.userModel.findOne({ id, role: 'student' });
-
-    return { student: user };
-  }
-
-  async findOneTeacher(id: string) {
-    const valid = isValidObjectId(id);
-    if (!valid) {
-      throw new BadRequestException('Invalid id');
-    }
-    const user = await this.userModel.findOne({ id, role: 'teacher' });
-
-    return { teacher: user };
   }
 }
